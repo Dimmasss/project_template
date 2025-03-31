5aab39a8e136cb18d3542254bc6c44f9a7624a26
Dmytro Sukailo <bogdansukailo00kx@gmail.com>
2025-03-31 23:24:03 +0300

    Added first test for function 3

--- tests/tests_io/test_input.py
+++ tests/tests_io/test_input.py
@@ -1,5 +1,6 @@
-from app.io.input import read_from_file_by_built_in_func
+from app.io.input import read_from_file_by_built_in_func, read_from_file_by_pandas
 import pytest
+import pandas as pd
 
 
 def test_read_from_empty_file():
@@ -35,3 +36,20 @@ def test_read_from_non_existent_file():
 
     with pytest.raises(FileNotFoundError):
         read_from_file_by_built_in_func(test_file_path)
+
+
+def test_read_from_file_by_pandas_if_exists():
+    # Arrange
+    csv_data = "Fruit,Vegetable,Number\nApple,Carrot,1\nBanana,Spinach,2\nCherry,Potato,3"
+    test_file = "test_data.csv"
+
+    with open(test_file, 'w') as file:
+        file.write(csv_data)
+
+    # Act
+    result = read_from_file_by_pandas(test_file)
+
+    # Assert
+    assert result is not None
+
+
