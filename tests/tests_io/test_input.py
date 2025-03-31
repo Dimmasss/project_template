6a0a3e2bdd7758ef35f9cdb6ebcf2c15ec39ac2c
Dmytro Sukailo <bogdansukailo00kx@gmail.com>
2025-03-31 23:16:18 +0300

    Added input test 2 and 3

--- tests/tests_io/test_input.py
+++ tests/tests_io/test_input.py
@@ -1,6 +1,7 @@
 from app.io.input import read_from_file_by_built_in_func
 import pytest
 
+
 def test_read_from_empty_file():
     # Arrange
     test_file_path = "tests/test_data/empty_file.txt"
@@ -13,3 +14,24 @@ def test_read_from_empty_file():
     # Assert
     expected = ""
     assert expected == actual
+
+
+def test_read_from_file_by_built_in_func():
+    # Arrange
+    test_file_path = "test_file.txt"
+    with open(test_file_path, "w") as file:
+        file.write("Hello, world!")
+
+    # Act
+    actual = read_from_file_by_built_in_func(test_file_path)
+
+    # Assert
+    expected = "Hello, world!"
+    assert expected == actual
+
+
+def test_read_from_non_existent_file():
+    test_file_path = "no_file.txt"
+
+    with pytest.raises(FileNotFoundError):
+        read_from_file_by_built_in_func(test_file_path)
