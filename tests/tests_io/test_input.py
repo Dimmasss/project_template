448b304287f2312952eabdaf7f8ee68e8aa3f8d3
Dmytro Sukailo <bogdansukailo00kx@gmail.com>
2025-03-31 23:33:23 +0300

    Added third test for function 3

--- tests/tests_io/test_input.py
+++ tests/tests_io/test_input.py
@@ -53,6 +53,25 @@ def test_read_from_file_by_pandas_if_exists():
     assert result is not None
 
 
-def test_read_from_file_with_pandas_non_file():
+def test_read_from_file_by_pandas_non_file():
     with pytest.raises(FileNotFoundError):
         read_from_file_by_pandas("non_file.csv")
+
+
+def test_read_from_file_with_pandas_content():
+    # Arrange
+    data = {
+        'Fruit': ['Apple', 'Banana', 'Cherry'],
+        'Vegetable': ['Carrot', 'Spinach', 'Potato'],
+        'Number': [1, 2, 3]
+    }
+    df_expected = pd.DataFrame(data)
+    test_file = "test_data1.csv"
+    df_expected.to_csv(test_file, index=False)
+
+    # Act
+    result = read_from_file_by_pandas(test_file)
+
+    # Assert
+    expected_result = "Fruit Vegetable  Number\n Apple  Carrot      1\n Banana  Spinach      2\n Cherry  Potato      3"
+    assert ' '.join(result.split()) == ' '.join(expected_result.split())
