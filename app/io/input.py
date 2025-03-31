eb942f169582b580fc759efd26c571968f2b41ac
Dmytro Sukailo <bogdansukailo00kx@gmail.com>
2025-03-31 21:25:50 +0300

    Implemented empty functions in input and output files

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def text_input_from_console():
+    pass
+
+
+def read_from_file_by_built_in_func(path_to_file):
+    pass
+
+
+def read_from_file_by_pandas(path_to_file):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_to_console(text):
+    pass
+
+
+def write_to_file_by_built_in_func(text, path_to_file):
+    pass
